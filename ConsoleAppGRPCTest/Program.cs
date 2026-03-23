8d12fde97b4746812918dbbe0bf6d7ea208a57c8
Russcazak <Russcazak@yandex.ru>
2026-03-23 18:03:42 +0500

    fix

--- ConsoleAppGRPCTest/Program.cs
+++ ConsoleAppGRPCTest/Program.cs
@@ -45,5 +45,24 @@ namespace ConsoleAppGRPCTest
                 }
             }
         }
+        public static async Task ReceiveFullServerStream()
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:6622");//63608:8081⁠
+            var client = new StationServiceGrpc.StationServiceGrpcClient(channel);
+            var request = new Empty();
+
+            using var call = client.SyncStationsAll(request);
+
+            await foreach (var response in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"Получено:  Network={response.Network}, OperatorCode={response.OperatorCode}, CountAdded={response.CountAdded}, CountSectors={response.CountSectors}," +
+                    $" ScannedSector={response.ScannedSector}," +
+                    $"Message={response.Message}, Time={response.Timestamp}");
+                if (response.IsDone)
+                {
+                    Console.WriteLine($"Получено: все");
+                }
+            }
+        }
     }
 }
