92cc732e7345c7afa464f575565d9b340849eb02
Russcazak <Russcazak@yandex.ru>
2026-03-26 17:03:37 +0500

    fix2

--- WebAppCellMapper/Services/IGeoBoundsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Concurrent;
-
-namespace WebAppCellMapper.Services
-{
-    public interface IGeoBoundsService
-    {
-        public ConcurrentQueue<SquareSearch> GetCoordianates(double latStart = GeoBoundsService.MIN_LAT, double latBorder = GeoBoundsService.MAX_LAT, double lonStart = GeoBoundsService.MIN_LON, double lonBorder = GeoBoundsService.MAX_LON, double step = GeoBoundsService.EFFECTIVE_STEP);
-    }
-}
--- WebAppCellMapper/Services/IProxyService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAppCellMapper.Services
-{
-    public interface IProxyService
-    {
-         Task GetProxies();
-         ProxyElement? GetProxy();
-         void ReleaseProxy(ProxyElement proxy);
-    }
-}
